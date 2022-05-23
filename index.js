934b4217042cb2ff989c75dd460989ea7a0a3041
Md Mazharul islam <miforbd@gmail.com>
2022-05-23 14:10:40 +0600

    post api purchase added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port=process.env.PORT||5000;
  
@@ -9,7 +10,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fkt9n.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -17,9 +18,22 @@ async function run(){
     try{
         await client.connect();
         const productCollection=client.db('StoreService').collection('product');
+        const purchaseCollection=client.db('StoreService').collection('purchase');
         app.get('/product',async(req,res)=>{
             const cursor= await productCollection.find().toArray();
             res.send(cursor)
+        });
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
+        // purchase 
+        app.post('/purchase',async(req,res)=>{
+            const newPurchase=req.body;
+            const result=await purchaseCollection.insertOne(newPurchase);
+            res.send(result);
         })
     }
     finally{
