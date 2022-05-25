126ee8ba215b080368edfccbddcf945987f5b95d
Md Mazharul islam <miforbd@gmail.com>
2022-05-25 13:12:55 +0600

    review show api created

--- index.js
+++ index.js
@@ -155,6 +155,11 @@ async function run() {
             const result = await reviewCollection.insertOne(review)
             res.send(result);
         })
+        // get review
+        app.get('/review', async (req, res) => {
+            const allReview = await reviewCollection.find().toArray();
+            res.send(allReview);
+        })
     }
     finally {
 
